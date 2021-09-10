0463c9a384ab8f6ac29b8c9b023ae5b9ace9790f
Vadym <vadimhodor@gmail.com>
2021-09-10 22:45:55 +0300

    domain models

---
+++ periodicals/src/main/java/ua/lviv/lgs/Lesson21TestApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.lgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson21TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson21TestApplication.class, args);
+	}
+
+}
